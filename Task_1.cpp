6445b847fa08998542548393624004a6afbf1820
Lucaschewitch <tsuyoiibakatoshii@gmail.com>
2024-12-15 21:29:41 +0300

    Add files via upload

---
+++ Task_1.cpp
@@ -0,0 +1,19 @@
+﻿#define _USE_MATH_DEFINES
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <locale>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "rus");
+    float x, y, R, S, C;
+    cin >> x >> y;
+    R = cos(2 * x) * sin(y);
+    S = pow(M_E, 1 / x) * log(y);
+    cout << R << endl << S << endl;
+    C = max(R, S);
+    cout << C << endl;
+    return 0;
+}
